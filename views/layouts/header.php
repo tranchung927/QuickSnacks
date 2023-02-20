b052d37434c33ae27a648283d0514ab2850b8bd4
ChungTV <chungtv.it.1001@gmail.com>
2023-02-20 20:08:54 +0700

    add header menu

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -1,1 +1,9 @@
-<?php
+<header id='header'>
+    <a href="index.php"><img src="public/images/ic_logo.svg" alt="Logo"/></a>
+    <ul class="header-menu">
+        <li><a href="index.php?controller=category">Thực đơn</a></li>
+        <li><a href="index.php?controller=promotion">Khuyến mãi</a></li>
+        <li><a href="index.php?controller=about">Giới thiệu</a></li>
+    </ul>
+</header>
+<div id="header_separator"></div>
