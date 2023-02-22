1a8f1c3b2b91a572fd01eeb480d1682ce5ed596b
ChungTV <chungtv.it.1001@gmail.com>
2023-02-22 15:10:36 +0700

    update home example data

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -10,6 +10,48 @@ class HomeController extends Controller {
       'name' => 'Chung',
       'age' => 29
     );
+    $products = array();
+
+    array_push($products, array(
+        'id' => 1,
+        'name' => 'Bánh ngô',
+        'description' => 'Đây là bánh ngô bà Tân vê lốc',
+        'price' => 18000.0,
+        'status' => 'active',
+        'calorie' => 300,
+        'estimate_time' => 20
+    ), array(
+        'id' => 2,
+        'name' => 'Bánh khoai',
+        'description' => 'Đây là bánh khoai bà Tân vê lốc',
+        'price' => 28000.0,
+        'status' => 'active',
+        'calorie' => 300,
+        'estimate_time' => 20
+    ), array(
+        'id' => 3,
+        'name' => 'Bánh sắn',
+        'description' => 'Đây là bánh sắn bà Tân vê lốc',
+        'price' => 58000.0,
+        'status' => 'active',
+        'calorie' => 300,
+        'estimate_time' => 20
+    ));
+    
+    
+   $categories = array();
+   array_push($categories, array(
+       'id' => 1,
+       'name' => 'Món mới',
+   ), array(
+       'id' => 2,
+       'name' => 'Món Khai Vị',
+   ), array(
+       'id' => 3,
+       'name' => 'Món Chính',
+   ));
+  
+    $data = array('products' => $products, 'categories' => $categories);
     $this->render('home', $data);
   }
 }
--- views/default/home.php
+++ views/default/home.php
@@ -1,4 +1,12 @@
-<?php
-  echo "Tên tôi là: $name, năm nay tôi $age tuổi";
-  echo '<br>';
-  echo '<a href="index.php?controller=cities">Cities</a>';
+<div>
+    <?php echo "Số lượng sản phẩm: ".count($products); ?>
+    <?php echo '<br>'; ?>
+    <?php echo "Số lượng danh mục: ".count($categories); ?>
+    <?php echo '<br>'; ?>
+    <?php echo '<a href="index.php?controller=cities">Cities</a>'; ?>
+    <?php echo '<br>'; ?>
+    <?php for ($i=0; $i < count($products); $i++){ ?>
+    <a href='index.php?controller=product&action=showProduct&id=<?php echo $products[$i]['id'] ?>'><?php echo $products[$i]['name'] ?></a>
+    <br>
+    <?php } ?>
+</div>
