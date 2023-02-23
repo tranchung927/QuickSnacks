abbc8178cfa2fb9c86095637e3a3531ae1d25b10
AnhTuongQuenEmRoi176 <122555544+AnhTuongQuenEmRoi176@users.noreply.github.com>
2023-02-23 20:45:05 +0700

    login and register

--- views/default/login.php
+++ views/default/login.php
@@ -3,26 +3,24 @@
         <h1>ĐĂNG NHẬP</h1>
         <div class="form-login">
             <div class="email">
-                <div class="line">
-                    <input type="text" placeholder="Địa chỉ email của bạn *" name="email" class="input-login">
-                </div>
-                <p style="color:red">Please enter your email address</p> 
+                <input type="text" placeholder="Địa chỉ email của bạn *" name="email" class="input-login">
+                <div class="line"></div>
+                <p style="color:red">Please enter your email address</p>
             </div>
             <div class="password">
-                <div class="line">
-                    <input type="password" placeholder="Mật khẩu *" name="password" class="input-login">
-                </div>
+                <input type="password" placeholder="Mật khẩu *" name="password" class="input-login">
+                <div class="line"></div>
                 <p style="color:red">Please enter your password</p> 
-            </div>
+            </div>  
         </div>
         <div class="forgot-password">
             <a href="" style="transition:none;color:black;text-decoration:none">Bạn quên mật khẩu?</a>
         </div>
         <div class="login-register">
-            <input type="text" value="ĐĂNG NHẬP" name="login" id="login-action">
+            <button name="login" id="login-action" href="#">ĐĂNG NHẬP</button>
         </div>
         <div class="button-newaccount">
-            <p>Bạn chưa có tài khoản? <a href="">Đăng ký</a></p>
+            <p>Bạn chưa có tài khoản? <a href="index.php?controller=user&action=register">Đăng ký</a></p>
         </div>
     </div>
 </form>
